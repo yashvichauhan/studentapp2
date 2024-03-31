61abe6f71146dbab4c33a76df6d3860305469172
yashvichauhan <31180761+yashvichauhan@users.noreply.github.com>
2024-03-31 00:26:34 -0400

    chaging public URL

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: "https://comp308studentapp2-mf.netlify.app/",
   },
 
   resolve: {
